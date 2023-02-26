147e59c6b131eeebeec838fcd2ff8ef2afe88372
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2023-02-26 23:11:55 +0530

    "Added employees"

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import 'reflect-metadata';
 import { typeormConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
   // imports: [UserModule],
-  imports: [UserModule, TypeOrmModule.forRoot(typeormConfig)]
+  imports: [UserModule, TypeOrmModule.forRoot(typeormConfig), EmployeesModule]
 })
 export class AppModule { }
