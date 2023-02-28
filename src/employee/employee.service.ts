688d97883529ce4013f3c0effcb07c9aa95945e0
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2023-02-28 09:27:39 +0530

    "Succesfully Implemented protected route"

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -15,45 +15,58 @@ export class EmployeeService {
     async register(dto: employeeRegisterDto): Promise<{ message: string, status: number }> {
         const hashedPassword = await bcrypt.hash(dto.password, 10);
         try {
-            const user = this.employeeRepository.create({ ...dto, password: hashedPassword });
-            await this.employeeRepository.save(user)
-            return { message: 'User registered successfully.', status: HttpStatus.CREATED };
+            const employee = this.employeeRepository.create({ ...dto, password: hashedPassword });
+            await this.employeeRepository.save(employee)
+            return { message: 'employee registered successfully.', status: HttpStatus.CREATED };
         } catch (error) {
-            throw new HttpException({ message: "Internal Server Error", error }, HttpStatus.INTERNAL_SERVER_ERROR)
+            throw new HttpException({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: "Internal Server Error", error }, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
 
     async login(dto: employeeLoginDto): Promise<{ token: string }> {
         const { email, password } = dto;
         try {
-            const user = await this.employeeRepository.findOne({ where: { email } });
-            if (!user) {
-                throw new HttpException({ statusCode: HttpStatus.NOT_FOUND, message: 'User not found' }, HttpStatus.NOT_FOUND);
+            const employee = await this.employeeRepository.findOne({ where: { email } });
+            if (!employee) {
+                throw new HttpException({ statusCode: HttpStatus.NOT_FOUND, message: 'employee not found' }, HttpStatus.NOT_FOUND);
             }
-            const isPasswordValid = bcrypt.compareSync(password, user.password);
+            const isPasswordValid = bcrypt.compareSync(password, employee.password);
             if (!isPasswordValid) {
                 throw new HttpException({ statusCode: HttpStatus.BAD_REQUEST, messgae: 'Incorrect password' }, HttpStatus.BAD_REQUEST);
             }
-            const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+            const token = jwt.sign({ id: employee._id, email: employee.email }, process.env.JWT_SECRET, {
                 expiresIn: '1h',
             });
             return { token };
 
         } catch (error) {
             if (JSON.stringify(error) === '{}') {
-                throw new HttpException({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: "Internal Server Error" }, HttpStatus.INTERNAL_SERVER_ERROR)
+                throw new HttpException({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: "Internal Server Error", error }, HttpStatus.INTERNAL_SERVER_ERROR)
             } else {
                 throw error;
             }
         }
     }
-    async getEmployee(token: string): Promise<Employee> {
+    async getEmployee(token: string): Promise<Employee | object> {
         try {
-            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-            const { id } = decodedToken as { id: ObjectID };
-            return await this.employeeRepository.findOne({ where: { _id: id } });
+            try {
+
+                const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+                const { id } = decodedToken as { id: ObjectID };
+                const employee = await this.employeeRepository.findOne({ where: { _id: id } });
+                if (!employee) {
+                    throw new HttpException({ statuscode: HttpStatus.UNAUTHORIZED, message: "Unauthorized Access" }, HttpStatus.UNAUTHORIZED)
+                }
+                return { employee, decodedToken }
+            } catch (error) {
+                throw new HttpException({ statuscode: HttpStatus.UNAUTHORIZED, message: "Invalid Token" }, HttpStatus.UNAUTHORIZED)
+            }
         } catch (error) {
-            throw new HttpException({ message: 'Invalid token', error }, HttpStatus.UNAUTHORIZED);
+            if (JSON.stringify(error) === '{}') {
+                throw new HttpException({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: "Internal Server Error", error }, HttpStatus.INTERNAL_SERVER_ERROR)
+            } else {
+                throw error;
+            }
         }
     }
     // async getAllEmployees(dto) { }
